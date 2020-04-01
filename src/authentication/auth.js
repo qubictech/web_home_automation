6792792605045054bab2938ae32b7d9d3f1db8dc
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-04-01 09:26:24 +0600

    login validation

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -2,7 +2,7 @@ function signInWithEmailPassword() {
     var email = document.getElementById("email").value;
     var pass = document.getElementById("pass").value;
 
-    if (email && pass) {
+    if (isValidEmailPass(email, pass)) {
         firebase.auth().signInWithEmailAndPassword(email, pass)
             .then(function (response) {
                 // User is signed in.
@@ -10,11 +10,32 @@ function signInWithEmailPassword() {
                 window.location.replace("../pages/dashboard.html")
             })
             .catch(function (error) {
-                window.alert(error.message);
+                window.alert("Login Failed! " + error.message);
+                console.log(error.message)
             });
-    } else {
-        window.alert("Email and password are required!");
     }
+}
+
+function isValidEmailPass(email, pass) {
+    let isValid = true;
+
+    atpos = email.indexOf("@");
+    dotpos = email.lastIndexOf(".");
+    var spaceCount = 0;
+    spaceCount = (pass.split(" ").length - 1);
+
+    if (!email && !pass) {
+        window.alert("Email and password can't be empty.");
+        isValid = false;
+    } else if (atpos < 1 || (dotpos - atpos < 2)) {
+        window.alert("Please enter a valid email address");
+        isValid = false;
+    } else if (pass.length < 6 || spaceCount > 0) {
+        window.alert("Password should be at least 6+ chars");
+        isValid = false;
+    }
+
+    return isValid;
 }
 
 function logout() {
